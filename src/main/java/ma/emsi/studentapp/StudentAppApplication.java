a56763837d9e0ec286e088c55e6a066bfd3ebb20
GramiSalma <salmagrami0@gmail.com>
2024-03-17 16:14:20 +0000

    Tester quelques fonctionnalites de recgherche

--- src/main/java/ma/emsi/studentapp/StudentAppApplication.java
+++ src/main/java/ma/emsi/studentapp/StudentAppApplication.java
@@ -20,9 +20,9 @@ public class StudentAppApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        productRepository.save(new Product(null,"Computer",4300,3));
-        productRepository.save(new Product(null,"Printer",1200,4));
-        productRepository.save(new Product(null,"Smartphone",3200,32));
+       // productRepository.save(new Product(null,"Computer",4300,3));
+       // productRepository.save(new Product(null,"Printer",1200,4));
+       // productRepository.save(new Product(null,"Smartphone",3200,32));
        List<Product> products=productRepository.findAll();
        products.forEach(p->{
            System.out.println(p.toString());
@@ -33,7 +33,26 @@ public class StudentAppApplication implements CommandLineRunner {
         System.out.println(product.getName());
         System.out.println(product.getQuantity());
         System.out.println("********************************");
+        System.out.println("----------------------------------");
+        List<Product> productList=productRepository.findByNameContains("C");
+        productList.forEach(p->{
+            System.out.println(p);
+        });
+        System.out.println("----------------------------------");
+        List<Product> productList2=productRepository.search("%C%");
+        productList2.forEach(p->{
+            System.out.println(p);
+        });
 
-
+        System.out.println("----------------------------------");
+        List<Product> productList3=productRepository.findByPriceGreaterThan(3000);
+        productList3.forEach(p->{
+            System.out.println(p);
+        });
+        System.out.println("----------------------------------");
+        List<Product> productList4=productRepository.searchByPrice(3000);
+        productList4.forEach(p->{
+            System.out.println(p);
+        });
     }
 }
